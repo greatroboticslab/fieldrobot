a6aa5942b1d15550c60665b32fdfc9e4fd1d9f38
festeringjackal <140579098+festeringjackal@users.noreply.github.com>
2023-09-07 12:29:10 -0500

    Updated cmdImg_node.cpp
    Fixed all of the minor bugs that came with the last patch; however, with the addition of proper image taking into the node, its processing speed has reduced tremendously. I am unsure of why this is, but I believe it has something to do with the axis_camera and its constituents. Will have to check again on a later date.

--- jaguar4x4_ROS_2014/src/cmdImg_node.cpp
+++ jaguar4x4_ROS_2014/src/cmdImg_node.cpp
@@ -7,8 +7,8 @@
 #include <boost/thread/thread.hpp>
 #include <ros/ros.h>
 #include <std_msgs/String.h>
-#include <fstream>
-#include <cassert>
+#include <fstream> // Required for file processing.
+#include <cassert> // Required to check if file exists.
 #include <iostream>
 #include <stdlib.h> // This is required for the system() call.
 #include <jaguar4x4_2014/GPSInfo.h> // This is required to process GPS data.
@@ -16,6 +16,26 @@
 #include <sys/stat.h> // This is required to verify the existence of certain files during processing.
 #include <chrono> // This is required for the timestamp on each command/image.
 
+
+/* READ ME FIRST BEFORE OPERATING NODE
+For the system() methods to operate properly, assuming you're running on 
+Ubuntu 20.04 (which you should be for ROS1 Noetic to work properly), you need to reconfigure your shell, which is currently configured to 
+/bin/dash, to /bin/bash, as you will otherwise get a message such as this: 
+
+"sh: 1: source: not found"
+
+As well as all the system() methods not functioning.
+
+To fix this, open up a new terminal window and enter in:
+
+"sudo dpkg-reconfigure dash"
+
+Enter in your password as normal.
+
+This will then ask whether you want dash to be your default system shell; 
+select "No", and then bash will be configured as your shell (or, /bin/sh
+will be pointing to /bin/bash).
+*/
 
 class cmdImgSync{
 
@@ -50,14 +70,14 @@ private:
 	double longiData;
 	double yawData;
 
-	string curCmnd = "N/A";
+	std::string curCmnd = "N/A";
 	bool cmndUpdated = false;
-	string curTime;
+	std::string curTime;
 
 	int counter = 0;
 	
-	fstream jaguarRecord;
-	fstream commandRecord;
+	std::fstream jaguarRecord;
+	std::fstream commandRecord;
 	
 	jaguar4x4_2014::GPSInfo gpsBuffer;
 	jaguar4x4_2014::IMUData imuBuffer;
@@ -71,21 +91,26 @@ private:
 
 // ------------------------------------------------------------
 
-void jagCmnd_cb(const std_msgs::String::ConstPtr& cmnd){
+void cmdImgSync::jagCmnd_cb(const std_msgs::String::ConstPtr& cmnd){
 	curCmnd = cmnd->data;
 	cmndUpdated = true;
 }
 
-void gps_cb(const jaguar4x4_2014::GPSInfo::ConstPtr& gpsInfo){
+void cmdImgSync::gps_cb(const jaguar4x4_2014::GPSInfo::ConstPtr& gpsInfo){
 	latiData = gpsInfo->latitude;
 	longiData = gpsInfo->longitude;
 }
 
-void imu_cb(const jaguar4x4_2014::IMUData::ConstPtr& imuInfo){
+void cmdImgSync::imu_cb(const jaguar4x4_2014::IMUData::ConstPtr& imuInfo){
 	yawData = imuInfo->yaw;
 }
 
 bool cmdImgSync::pubReady(){
+	bool check1 = false
+	bool check2 = false
+	bool check3 = false
+	
+	
 	if (motor_cmd_sub_.getNumPublishers() < 1){
 		return false;
 	} else { return true; }
@@ -111,43 +136,46 @@ std::string cmdImgSync::timeConverter(std::chrono::milliseconds timeSinceEpoch){
 }
 
 void cmdImgSync::updateData(){
-	jaguarRecord.open("/home/jackal/catkin_rbt_ws/src/jaguar4x4_ROS_2014/src/jaguarRecord.dat", ios::app);
-	commandRecord.open("/home/jackal/catkin_rbt_ws/src/jaguar4x4_ROS_2014/src/commandRecord.dat", ios::app);
+	jaguarRecord.open("/home/jackal/catkin_rbt_ws/src/jaguar4x4_ROS_2014/src/jaguarRecordCopy.dat", std::ios::app);
+	commandRecord.open("/home/jackal/catkin_rbt_ws/src/jaguar4x4_ROS_2014/src/commandRecord.dat", std::ios::app);
 
 	assert(jaguarRecord);
-        assert(commandRecord);
+	assert(commandRecord);
 	
 	tse = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch());
-	curTime = this.timeConverter(tse);
+	curTime = this->timeConverter(tse);
 
 	if (cmndUpdated){
 		jaguarRecord << curCmnd << ", ";
-		commandRecord << curTime << ", " << counter << "|" << curCmnd << endl;
-		updated = false;
+		commandRecord << curTime << ", " << counter << "|" << curCmnd << std::endl;
+		cmndUpdated = false;
 	} else {
 		jaguarRecord << "No current command, ";
-		commandRecord << curTime << ", " << counter << "|" << "NO_CHANGE" << endl;
+		commandRecord << curTime << ", " << counter << "|" << "NO_CHANGE" << std::endl;
 	}
 
-	jaguarRecord << fixed << setprecision(13); // This is set to 13 decimal points of precision, after the decimal point, to keep the GPS data accurate.
+	jaguarRecord << std::fixed << std::setprecision(13); // This is set to 13 decimal points of precision, after the decimal point, to keep the GPS data accurate.
 	jaguarRecord << yawData << ", " << latiData << ", " << longiData << ", "; 
-	jaguarRecord << 0 << endl; // This line is simply for the altitude, which the robot does not record, but is required for the GUI program.
+	jaguarRecord << 0 << std::endl; // This line is simply for the altitude, which the robot does not record, but is required for the GUI program.
 
 	// This takes a photo and saves it to the default saving location. This is still a bit buggy. Working on some fixes at the moment.
 	std::system("source /opt/ros/noetic/setup.bash && source ~/.bashrc && cd ~/catkin_rbt_ws/ && source devel/setup.bash && rosservice call /image_saver/save");
 	counter++;
+	
+	jaguarRecord.close();
+	commandRecord.close();
 }
 
 // ------------------------------------------------------------
 
-int main(){
+int main(int argc, char** argv){
 	// Initializing the node.
-	ros::init(argc, argv, "cmdImg_node", ros::init_options::AnonymousName | ros::init_options::NoSigintHandler);
+	ros::init(argc,argv,"cmdImg_node", ros::init_options::AnonymousName | ros::init_options::NoSigintHandler);
 	
 	cmdImgSync syncer;
 	
-	std::system("source /opt/ros/noetic/setup.bash && source ~/.bashrc && cd ~/catkin_rbt_ws/ && source devel/setup.bash && roslaunch axis_camera axis.launch hostname:=192.168.0.65:8081 username:=root password:=password encrypted:=true");
-	std::system("source /opt/ros/noetic/setup.bash && source ~/.bashrc && cd ~/catkin_rbt_ws/ && source devel/setup.bash && rosrun image_view image_saver image:=/axis/image_raw _image_transport:=compressed _filename_format:=fubar%04i.jpg _save_all_image:=false __name:=image_saver");
+	std::system("gnome-terminal -- sh -c \"source /opt/ros/noetic/setup.bash && source ~/.bashrc && cd ~/catkin_rbt_ws/ && source devel/setup.bash && roslaunch axis_camera axis.launch hostname:=192.168.0.65:8081 username:=root password:=drrobot encrypted:=true\"");
+	std::system("gnome-terminal -- sh -c \"source /opt/ros/noetic/setup.bash && source ~/.bashrc && cd ~/catkin_rbt_ws/ && source devel/setup.bash && rosrun image_view image_saver image:=/axis/image_raw _image_transport:=compressed _filename_format:=fubar%04i.jpg _save_all_image:=false __name:=image_saver\"");
 	
 	// Loop until the jaguar4x4_2014_node node responds
 	while(!syncer.pubReady());
@@ -155,7 +183,7 @@ int main(){
 	ros::Rate loopRate(50);
 	
 	while(syncer.pubReady()){
-		cmdImgSync.updateData();
+		syncer.updateData();
 		
 		// Spinning the node once
 		ros::spinOnce();
