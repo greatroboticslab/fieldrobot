581accd2e40d7b1d91ccde7c66bc0eaefcd735b8
festeringjackal <140579098+festeringjackal@users.noreply.github.com>
2023-09-21 12:42:42 -0500

    Updated cmdImg_node
    Added comments to the code. Still unfinished.
    
    Fixed a small output bug affecting commandImage.dat.

--- jaguar4x4_ROS_2014/src/cmdImg_node.cpp
+++ jaguar4x4_ROS_2014/src/cmdImg_node.cpp
@@ -1,34 +1,35 @@
-#include <signal.h>
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/poll.h>
-#include <sstream> // Required for processing stringstream
-#include <boost/thread/thread.hpp> // Required for multithreading
-#include <ros/ros.h> // Required for ros
-#include <std_msgs/String.h> // Required for ros
+#include <iostream> // Required for standard in and out.
+#include <stdio.h> // Required to perform input/output operations.
+#include <stdlib.h> // Required for the system() call.
+#include <sstream> // Required for processing stringstream.
+#include <boost/thread/thread.hpp> // Required for multithreading.
+#include <ros/ros.h> // Required for ROS.
+#include <std_msgs/String.h> // Required for ROS standard message handling and processing.
 #include <fstream> // Required for file processing.
 #include <cassert> // Required to check if file exists.
-#include <iostream> // Required for standard in and out
-#include <stdlib.h> // This is required for the system() call.
-#include <jaguar4x4_2014/GPSInfo.h> // This is required to process GPS data.
-#include <jaguar4x4_2014/IMUData.h> // This is required to process IMU data.
-#include <chrono> // This is required for the timestamp on each command/image.
+#include <jaguar4x4_2014/GPSInfo.h> // Required to handle and process GPS message data from the Jaguar robot.
+#include <jaguar4x4_2014/IMUData.h> // Required to handle and process IMU message data from the Jaguar robot.
+#include <chrono> // Required for the timestamp on each command/image.
 
 
-// This all handles image processing.
-// cv_bridge and opencv2 need to be installed manually
+/* The preprocessor directories below all handle image processing and 
+handling */
+/* cv_bridge and opencv2 need to be installed manually. They are 
+required to process the images from the Jaguar robot; cv_bridge 
+is used specifically to convert between ROS and OpenCV images. */
 #include <cv_bridge/cv_bridge.h> // https://github.com/ros-perception/vision_opencv.git
-#include <opencv2/imgproc/imgproc.hpp> // https://docs.opencv.org/4.x/d7/d9f/tutorial_linux_install.html or https://www.geeksforgeeks.org/how-to-install-opencv-in-c-on-linux/ (MAKE SURE TO UPDATE DIRECTORY IN CMAKELISTS.TXT. WILL SAVE YOU A LOT OF TIME.)
+#include <opencv2/imgproc/imgproc.hpp> // https://docs.opencv.org/4.x/d7/d9f/tutorial_linux_install.html or https://www.geeksforgeeks.org/how-to-install-opencv-in-c-on-linux/
 #include <opencv2/opencv.hpp>
-#include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/image_encodings.h> // Required for ROS standard message handling and processing.
 #include <image_transport/image_transport.h> // https://github.com/ros-perception/image_common.git
 
 
 /* READ ME FIRST BEFORE OPERATING NODE
 For the system() methods to operate properly, assuming you're running on 
-Ubuntu 20.04 (which you should be for ROS1 Noetic to work properly), you need to reconfigure your shell - which is currently configured to 
-/bin/dash - to /bin/bash, as you will otherwise get a message such as this: 
+Ubuntu 20.04 (which you should be for ROS1 Noetic to work properly), you 
+need to reconfigure your shell - which is currently configured to 
+/bin/dash - to /bin/bash, as you will otherwise get a message such as 
+this: 
 
 "sh: 1: source: not found"
 
@@ -48,94 +49,164 @@ will be pointing to /bin/bash).
 Keep in mind that the jaguarRecord.dat, commandImage.dat, and 
 commandRead.dat files should be emptied before running, unless you want 
 older data mixed in with the new data. The counter should make it easier 
-to distinguish when one record ends and when another begins, when 
-applicable.
+to distinguish when one record ends and when another begins in the same, 
+file, when applicable.
 */
 
+/* Defining cmdImgSync class. This will be used to perform all data 
+handling and processing. */
 class cmdImgSync{
 
 public:
+	/* Class constructor for cmdImgSync. The "ih(nh)" seen here is 
+	an initialization list, used to initialize the ImageTransport 
+	object (ih, standing for Image Handler) with the NodeHandle 
+	object (nh, standing for Node Handler) */
 	cmdImgSync() : ih(nh){
-	
+		
+		// Subscribing to the Jaguar's motor commands topic.
 		motor_cmd_sub_ = nh.subscribe<std_msgs::String>("drrobot_motor_cmd", 1, boost::bind(&cmdImgSync::jagCmnd_cb, this, _1));
 		
+		// Subscribing to the Jaguar's GPS data topic.
 		gpsData = nh.subscribe<jaguar4x4_2014::GPSInfo>("drrobot_gps", 1, boost::bind(&cmdImgSync::gps_cb, this, _1));
 		
+		// Subscribing to the Jaguar's IMU data topic.
 		imuData = nh.subscribe<jaguar4x4_2014::IMUData>("drrobot_imu", 1, boost::bind(&cmdImgSync::imu_cb, this, _1));
 		
+		/* Subscribing to the Jaguar's camera topic, provided 
+		by the axis_camera package and its constituents. */
 		imgData = ih.subscribe("axis/image_raw", 1, boost::bind(&cmdImgSync::img_cb, this, _1));
 	}
 	
-	~cmdImgSync(){}
+	~cmdImgSync(){} // Class destructor for cmdImgSync
 	
+	/* Declaring function prototypes below... */
+	/* If you do not understand ROS callback functions, check here
+	for more information http://wiki.ros.org/roscpp/Overview/ */
+	
+	/* jagCmnd_cb is the callback function for whenever the 
+	Jaguar's motor command topic updates. */
 	void jagCmnd_cb(const std_msgs::String::ConstPtr& cmnd);
+	
+	/* gps_cb is the callback function for whenever the Jaguar's 
+	GPS data topic updates. */
 	void gps_cb(const jaguar4x4_2014::GPSInfo::ConstPtr& gpsInfo);
+	
+	/* imu_cb is the callback function for whenever the Jaguar's 
+	IMU data topic updates. */
 	void imu_cb(const jaguar4x4_2014::IMUData::ConstPtr& imuInfo);
+	
+	/* img_cb is the callback function for whenever the Jaguar's 
+	camera posts a new image on the axis/image_raw topic. */
 	void img_cb(const sensor_msgs::ImageConstPtr& img);
 	
+	/* updateData writes any new command/GPS/IMU data recieved 
+	from the callback functions to the jaguarRecord.dat and 
+	commandRecord.dat files. */
 	void updateData();
-
+	
+	/* timeConverter converts the current time since epoch to 
+	a string timestamp that can be easily understood. */
 	std::string timeConverter(std::chrono::milliseconds timeSinceEpoch);
+	
+	/* pubReady checks whether all the ROS topics that the 
+	subscribers are subscribed to have active publishers 
+	publishing to them; pubReady prevents the cmdImg_node from 
+	completely activating until the drrobot_player node is 
+	ready. */
 	bool pubReady();
 	
 	
 private:
-	ros::NodeHandle nh;
-	image_transport::ImageTransport ih;
-	ros::Subscriber motor_cmd_sub_; // Same name as in drrobot_player.cpp for clarity
-	ros::Subscriber gpsData;
-	ros::Subscriber imuData;
-	image_transport::Subscriber imgData;
+	ros::NodeHandle nh; // NodeHandle to initialize ROS subscribers.
+	image_transport::ImageTransport ih; /* ImageTransport to 
+	initialize the image_transport subscriber. */
+	ros::Subscriber motor_cmd_sub_; /* Jaguar motor command 
+	subscriber. Same name as in drrobot_player.cpp for clarity */
+	ros::Subscriber gpsData; // Jaguar GPS data subscriber. 
+	ros::Subscriber imuData; // Jaguar IMU data subscriber.
+	image_transport::Subscriber imgData; /* Jaguar camera image 
+	subscriber */
 	
-	double latiData;
-	double longiData;
-	double yawData;
+	double latiData; // Variable to hold latitude data.
+	double longiData; // Variable to hold longitude data.
+	double yawData; // Variable to hold yaw data.
 
-	std::string curCmnd = "N/A";
-	bool cmndUpdated = false;
-	std::string curTime;
+	std::string curCmnd = "NO_CHANGE"; /* Variable to hold the 
+	current Jaguar command. */
+	bool cmndUpdated = false; /* Variable to indicate whether a 
+	new command has been recieved or not. If true, indicates that 
+	a new command (different from the last) has came in. */
+	std::string curTime; /* Variable to hold the current time 
+	calculated from the timeConverter function */
 
-	int counter = 0;
+	int counter = 0; /* Variable that indicates the current command 
+	and image pair in the commandRecord.dat and commandImage.dat 
+	files. */
 	
+	/* File stream variables for each respective .dat file, used 
+	to append to each respective .dat file with new data. */
 	std::fstream jaguarRecord;
 	std::fstream commandRecord;
 	std::fstream commandImage;
 	
-	std::chrono::milliseconds tse;
-	int msEpoch;
-	int secEpoch;
-	int minEpoch;
-	int hrEpoch;
+	// These are all used for the timeConverter.
+	std::chrono::milliseconds tse; // Time since epoch in ms.
+	int msEpoch; /* The current millisecond from the tse. */
+	int secEpoch; /* The current second from the tse. */
+	int minEpoch; /* The current minute from the tse. */
+	int hrEpoch; /* The current hour from the tse. */
 };
 
 // ------------------------------------------------------------
 
+/* (CALLBACK) This function updates curCmnd and cmndUpdated whenever 
+a new Jaguar command is recieved. */
 void cmdImgSync::jagCmnd_cb(const std_msgs::String::ConstPtr& cmnd){
-	curCmnd = cmnd->data;
-	cmndUpdated = true;
+	curCmnd = cmnd->data; /* Updating curCmnd with the new Jaguar 
+	command. */
+	cmndUpdated = true; /* Setting cmndUpdated to true, indicating 
+	a new command has been recieved. */
 }
 
+/* (CALLBACK) This function updates latiData and longiData whenever the 
+Jaguar updates its latitude and longitude data. */
 void cmdImgSync::gps_cb(const jaguar4x4_2014::GPSInfo::ConstPtr& gpsInfo){
-	latiData = gpsInfo->latitude;
-	longiData = gpsInfo->longitude;
+	latiData = gpsInfo->latitude; /* Updating latiData with the new 
+	Jaguar latitude data. */
+	longiData = gpsInfo->longitude; /* Updating longiData with the 
+	new Jaguar longitude data. */
 }
 
+/* (CALLBACK) This function updates yawData whenever the Jaguar updates 
+its yaw data. */
 void cmdImgSync::imu_cb(const jaguar4x4_2014::IMUData::ConstPtr& imuInfo){
-	yawData = imuInfo->yaw;
+	yawData = imuInfo->yaw; /* Updating yawData with the new Jaguar
+	yaw data. */
 }
 
+/* (CALLBACK) This function saves any new images that the Jaguar's 
+camera collects to the specified file destination (fileDest) It 
+handles all of the image handling and processing. */
 void cmdImgSync::img_cb(const sensor_msgs::ImageConstPtr& img){
+	// Opening commandImage.dat to append mode to add new data.
 	commandImage.open("/home/jackal/catkin_rbt_ws/src/jaguar4x4_ROS_2014/src/commandImage.dat", std::ios::app);
 	
-	assert(commandImage);
+	assert(commandImage); /* Asserting commandImage to ensure file
+	exists */
 	
+	// Setting tse to the current time since epoch.
 	tse = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch());
-	curTime = this->timeConverter(tse);
 	
+	curTime = this->timeConverter(tse); /* Creating a timestamp, 
+	using timeConverter to convert tse. */
+	
+	/* Setting the file destination, using curTime as the name of 
+	the image file. */
 	std::string fileDest = "/home/jackal/catkin_rbt_ws/src/jaguar4x4_ROS_2014/src/camera_images/" + curTime + ".png";
 		
-	// Converting a sensor_msgs::Image message into a CVImage
-	cv_bridge::CvImagePtr cv_image;
+	cv_bridge::CvImagePtr cv_image; /* Converting a 
+	sensor_msgs::Image message into a CVImage */
 	
 	cv_image = cv_bridge::toCvCopy(img);
 	
@@ -149,12 +220,12 @@ void cmdImgSync::img_cb(const sensor_msgs::ImageConstPtr& img){
 	accurate, as it is recording at 30 Hz (due to the 30 FPS 
 	camera). Using the if-else statement for the replay may result 
 	in missing motor commands. */
-	// commandImage << std::setw(18) << curTime + ".png" << counter << "|" << curCmnd << std::endl;
+	// commandImage << std::setw(100) << curTime + ".png" << counter << "|" << curCmnd << std::endl;
 	
 	if (cmndUpdated){
-		commandImage << std::setw(18) << fileDest << counter << "|" << curCmnd << std::endl;
+		commandImage << std::setw(100) << fileDest << counter << "|" << curCmnd << std::endl;
 	} else {
-		commandImage << std::setw(18) << fileDest << counter << "|" << "NO_CHANGE" << std::endl;
+		commandImage << std::setw(100) << fileDest << counter << "|" << "NO_CHANGE" << std::endl;
 	}
 	
 	commandImage.close();
